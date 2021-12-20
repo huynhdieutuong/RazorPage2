1dba3a20f3d593aee2ca2c137ce634beae13b402
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2021-12-20 15:49:26 +0700

    Upload single file

--- Pages/Contact.cshtml.cs
+++ Pages/Contact.cshtml.cs
@@ -1,7 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -11,7 +16,20 @@ namespace RazorPage2.Pages
     {
         [BindProperty]
         public CustomerInfo CustomerInfo { get; set; }
+
+        [BindProperty]
+        [DataType(DataType.Upload)]
+        [Required(ErrorMessage = "Choose a file to upload")]
+        [DisplayName("File upload")]
+        public IFormFile FileUpload { get; set; }
+
         public string Message { get; set; }
+        private readonly IWebHostEnvironment _environment;
+
+        public ContactModel(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
         public void OnGet()
         {
         }
@@ -21,6 +39,16 @@ namespace RazorPage2.Pages
             if (ModelState.IsValid)
             {
                 Message = "Data is valid";
+
+                if (FileUpload != null)
+                {
+                    // Create filepath to save file
+                    var filepath = Path.Combine(_environment.WebRootPath, "uploads", FileUpload.FileName);
+                    // Create filestream to write file
+                    using var filestream = new FileStream(filepath, FileMode.Create);
+                    // Copy file to filestream
+                    FileUpload.CopyTo(filestream);
+                }
             }
             else
             {
