e46b0e5581c0da964fa8bbde235c94796ca02c09
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2021-12-20 12:39:48 +0700

    Validate data

--- Models/CustomerInfo.cs
+++ Models/CustomerInfo.cs
@@ -5,9 +5,16 @@ using Microsoft.AspNetCore.Mvc;
 public class CustomerInfo
 {
     [Display(Name = "Customer name")]
+    [Required(ErrorMessage = "{0} is required")]
+    [StringLength(20, MinimumLength = 3, ErrorMessage = "{0} must be between {2}-{1} characters")]
     public string CustomerName { get; set; }
+
     [DisplayName("Email address")]
+    [Required(ErrorMessage = "{0} is required")]
+    [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "{0} is not valid")]
     public string Email { get; set; }
+
     [Display(Name = "Year of birth")]
+    [Range(1970, 2000, ErrorMessage = "{0} must between {1}-{2}")]
     public int? YearOfBirth { get; set; }
 }
