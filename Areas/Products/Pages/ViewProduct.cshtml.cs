a10a034fe17306da11a407a3ffdcb304a3ee6581
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2021-12-20 10:53:47 +0700

    Model Binding: Parameter Binding & Propety Binding

--- Areas/Products/Pages/ViewProduct.cshtml.cs
+++ Areas/Products/Pages/ViewProduct.cshtml.cs
@@ -16,9 +16,10 @@ public class ViewProductModel : PageModel
         _productListService = productListService;
     }
 
-    public void OnGet(int? id)
+    public void OnGet(int? id, [Bind("Id", "Name")] Product product)
     {
-        // 1. Request.RouteValues["id"]
+        // 1. Read data directly by this.Request
+        // 1.1 Request.RouteValues["id"]
         // if (Request.RouteValues["id"] != null)
         // {
         //     int id = int.Parse(Request.RouteValues["id"].ToString());
@@ -30,7 +31,29 @@ public class ViewProductModel : PageModel
         //     ViewData["Title"] = $"Product List";
         // }
 
-        // 2. OnGet(int? id)
+        // 1.2
+        // var form = this.Request.Form["id"];
+        // var queryId = this.Request.Query["id"];
+        // var data = this.Request.RouteValues["id"];
+        // var header = this.Request.Headers["user-agent"];
+        // if (data != null)
+        // {
+        //     System.Console.WriteLine(queryId);
+        //     System.Console.WriteLine(header);
+        //     ProductId = int.Parse(data.ToString());
+        //     ViewData["Title"] = $"Product {ProductId}";
+        //     Product = _productListService.Find(ProductId);
+        // }
+
+        // 2. Model Binding by Attributes:
+        // [FromQuery] => OnGet([FromQuery] int? id) => /products/343223?id=4
+        //             => OnGet([FromQuery(Name = "productId")] int? id) => /products/343223?productId=4
+        // [FromRoute] => OnGet([FromRoute] int? id)
+        // [FromForm]
+        // [FromHeader]
+        // [FromBody]
+        // 2.1 Parameter Binding: 
+        // 2.1.1 OnGet(int? id)
         if (id != null)
         {
             ProductId = id.Value;
@@ -41,6 +64,19 @@ public class ViewProductModel : PageModel
         {
             ViewData["Title"] = $"Product List";
         }
+
+        // 2.1.2 OnGet(int? id, Product product)
+        // /products/3?Id=2&Name=Iphone&Price=100 => Id = 3, Name = Iphone, Price = 100
+        System.Console.WriteLine($"ID: {product.Id}");
+        System.Console.WriteLine($"Name: {product.Name}");
+        System.Console.WriteLine($"Price: {product.Price}");
+
+        // 2.1.3 OnGet(int? id, [Bind("Id", "Name")] Product product) => Only bind Id, Name
+        // /products/3?Id=2&Name=Iphone&Price=100 => Id = 3, Name = Iphone, Price = 0
+
+        // 2.2 Property Binding
+        // Code in Privacy.cshtml.cs
+
     }
 
     // /products/{item.Id}?handler=delete
--- Pages/Privacy.cshtml.cs
+++ Pages/Privacy.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -10,6 +11,16 @@ namespace RazorPage2.Pages
 {
     public class PrivacyModel : PageModel
     {
+        // [BindProperty] - Default binding in POST
+        // [BindProperty(SupportsGet = true)] - Support binding in GET
+        // /privacy.html?UserId=abc&Email=tuong@gmail.com&UserName=Tuong
+        [BindProperty]
+        [DisplayName("Your Id")]
+        public string UserId { get; set; }
+        [BindProperty]
+        public string Email { get; set; }
+        [BindProperty]
+        public string UserName { get; set; }
         private readonly ILogger<PrivacyModel> _logger;
 
         public PrivacyModel(ILogger<PrivacyModel> logger)
@@ -19,6 +30,12 @@ namespace RazorPage2.Pages
 
         public void OnGet()
         {
+        }
+        public void OnPost()
+        {
+            System.Console.WriteLine(UserId);
+            System.Console.WriteLine(Email);
+            System.Console.WriteLine(UserName);
         }
     }
 }
